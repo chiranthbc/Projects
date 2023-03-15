74b5e51698303b2ddf5a6f89838e1bede8b70d5b
CHIRANTH <CHIRANTH@LAPTOP-A2BPHAFN>
2023-03-15 09:41:06 +0530

    First Commit

---
+++ HotelInventoryMs/src/main/java/com/chiranth/HotelInventoryMs/HotelInvent/package-info.java
@@ -0,0 +1,1 @@
+package com.chiranth.HotelInventoryMs.HotelInvent;
---
+++ HotelInventoryMs/src/test/java/com/chiranth/HotelInventoryMs/HotelInventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiranth.HotelInventoryMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelInventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
